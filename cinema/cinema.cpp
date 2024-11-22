3c1130545a79e1dca51fd49a42ac5bc37dcb8685
kanashimi <kirill2759418@gmail.com>
2024-11-22 13:57:08 +0300

    Добавьте файлы проекта.

---
+++ cinema/cinema.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int old_mode = _setmode(_fileno(stdout), _O_U16TEXT);
+
+
+    // Выполняем Unicode-вывод
+    wcout << L"Йоу, ╬†♂\n";
+    // Возвращаем исходный режим
+    _setmode(_fileno(stdout), old_mode);
+
+
+    cout << "Йоу" << endl;
+}
