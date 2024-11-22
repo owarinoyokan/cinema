50fe4e52a03a59e673292de1d8e97805878b3a8a
kanashimi <kirill2759418@gmail.com>
2024-11-22 14:31:04 +0300

    update

--- cinema/cinema.cpp
+++ cinema/cinema.cpp
@@ -3,16 +3,20 @@
 #include <fcntl.h>
 using namespace std;
 
-int main() {
-    setlocale(LC_ALL, "ru");
+//Функция для вывода alt-кода (широкие символы типа wchar_t)
+void Wout() {
     int old_mode = _setmode(_fileno(stdout), _O_U16TEXT);
-
-
     // Выполняем Unicode-вывод
-    wcout << L"Йоу, ╬†♂\n";
+    wcout << L"Заготовка под вывод alt-кода.\n╬†♂\n";
     // Возвращаем исходный режим
     _setmode(_fileno(stdout), old_mode);
+}
 
+int main() {
+    setlocale(LC_ALL, "ru");
+    
+    Wout();
 
-    cout << "Йоу" << endl;
+    cout << "\nЙоу" << endl;
+    cout << "\nПобеда" << endl;
 }
